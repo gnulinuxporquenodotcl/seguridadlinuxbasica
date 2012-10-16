4377efe322cc3023971de93c8139ae4999d1e68c
ed00m:th3d00m <ed00m@gnulinuxporqueno.cl>
2012-10-16 04:06:21 -0300

    ejemplo: buscar archivos con owner diferente al usuario ingresado

---
+++ ejemplos/find-001-dist-owner.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -x
+set -e
+
+USER=${1:?INGRESA NOMBRE DEL USUARIO}
+
+###########
+#
+# ESTE SCRIPT BUSCA LOS ARCHIVOS CON OWNER DISTINTO AL USUARIO QUE SE INGRESO
+#
+##########
+
+USEREXEC=`whoami`
+
+if [ ${USEREXEC} != "root" ];then
+    echo "* EJECUTA ESTE SCRIPT COMO ROOT PARA OBTENER MAS RESULTADOS"
+else
+    find / ! -user ${USER} -exec ls -ltrh {} \;
+fi
+exit 0
